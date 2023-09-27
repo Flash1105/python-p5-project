a7794ac3d29765ac4a58cfcfd3d84cb8888c9001
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-09-27 16:54:40 -0400

    add log in feature

--- auth/routes.py
+++ auth/routes.py
@@ -1,6 +1,20 @@
-from flask import render_template
+from flask import render_template, Blueprint, request, redirect, url_for, session, flash
 from . import bp  
+from werkzeug.security import check_password_hash
+from .forms import LoginForm, RegisterForm
+from server.models import User
 
-@bp.route('/login')
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password_hash, form.password.data):
+            session['user_id'] = user.user_id
+            flash('You were successfully logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password', 'danger')
+    return render_template('login.html', form=form)
