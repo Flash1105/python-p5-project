9898d9f1963c12714a9b6400faa0e5af9ed41953
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-09-27 16:58:36 -0400

    add register feature

--- auth/routes.py
+++ auth/routes.py
@@ -25,3 +25,13 @@ def logout():
     flash('You were successfully logged out!', 'success')
     return redirect(url_for('main.index'))
 
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You were successfully registered!', 'success')
+    return redirect(url_for('auth.login'))
