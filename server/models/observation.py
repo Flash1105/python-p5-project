6f5e182fd8c78839eefe887e94dfd9eec049aeb2
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-09-28 00:41:22 -0400

    add primary key

--- server/models/observation.py
+++ server/models/observation.py
@@ -2,11 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from server.database import db
 
 class Observation(db.Model):
-
+    id = db.Column(db.Integer, primary_key=True)
     def __init__(self, species, location, behavior, user_id, images):
         self.species = species
         self.location = location
         self.behavior = behavior
         self.user_id = user_id
         self.images = images
-        
