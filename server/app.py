397e9ebb57b17f802ed9663f6d9bb5deaaa00a66
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-09-28 06:39:13 -0400

    break app into 2 for better orginization

--- server/app.py
+++ server/app.py
@@ -1,32 +1,17 @@
-import os
 from flask import Flask, render_template
 from flask_migrate import Migrate
-from server.database import db
-from auth.routes import auth_bp, observation_bp
-from flask_login import LoginManager
+from server.config import Config
+from .database import db
 
 app = Flask(__name__, template_folder='../templates')
-app.register_blueprint(auth_bp, url_prefix='/auth.')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'a_default_key')
-app.register_blueprint(observation_bp, url_prefix='/observation')
-
-# Define the base directory
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-# Check if the directory exists and if not, creat it
-db_directory = os.path.join(BASE_DIR, 'data')
-if not os.path.exists(db_directory):
-    os.makedirs(db_directory)
-
-# Configure the SQLAlchemy Database URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(db_directory, 'app.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(Config)
 
 db.init_app(app)
-
 migrate = Migrate(app, db)
 
-
+from auth.routes import auth_bp, observation_bp
+app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(observation_bp, url_prefix='/observation')
 
 @app.route('/')
 def index():
--- server/config.py
+++ server/config.py
@@ -1,28 +1,14 @@
-# Standard library imports
+import os
 
-# Remote library imports
-from flask import Flask
-from flask_cors import CORS
-from flask_migrate import Migrate
-from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+class Config:
+    SECRET_KEY = os.environ.get('secret_key', 'secret')
 
-# Local imports
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-# Instantiate app, set attributes
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-# Define metadata, instantiate db
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+    DB_DIRECTORY =os.path.join(BASE_DIR, 'data')
+    if not os.path.exists(DB_DIRECTORY):
+        os.makedirs(DB_DIRECTORY)
 
-# Instantiate REST API
-api = Api(app)
-
-# Instantiate CORS
-CORS(app)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DB_DIRECTORY, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
