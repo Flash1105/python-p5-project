68451027d37b9d427fccdc80962d1d5171c5dcd5
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-09-28 05:30:00 -0400

    properly import database

--- server/models.py
+++ server/models.py
@@ -3,7 +3,7 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from database import db
 
 # Models go here!
 
