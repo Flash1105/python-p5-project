48b85cbbb54d3259a790db1cc6ce0db0f184b3d4
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-09-28 01:58:17 -0400

    small symbol error fix

--- server/models/discussion.py
+++ server/models/discussion.py
@@ -1,13 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from server.database import db
 
-
 class Discussion(db.Model):
     __tablename__ = 'discussions'
 
     discussion_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    observation_id = db.Column(db.Integer, db.ForeignKey('observation_id'), nullable=False)
+    observation_id = db.Column(db.Integer, db.ForeignKey('observations.id'), nullable=False)
     message = db.Column(db.Text, nullable=False)
 
-    observation = db.relationship('Observation', back_populates='discussion')
+    observation = db.relationship('Observation', back_populates='discussions')
