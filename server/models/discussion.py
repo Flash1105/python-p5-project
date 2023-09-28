790f3df78d87239e1bccbca4d60fddf38f299ac7
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-09-28 01:23:49 -0400

    remove user db relationship

--- server/models/discussion.py
+++ server/models/discussion.py
@@ -1,13 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from server.database import db
 
+
 class Discussion(db.Model):
     __tablename__ = 'discussions'
 
     discussion_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    observation_id = db.Column(db.Integer, db.ForeignKey('observations.observation_id'), nullable=False)
+    observation_id = db.Column(db.Integer, db.ForeignKey('observation_id'), nullable=False)
     message = db.Column(db.Text, nullable=False)
 
-    user = db.relationship('User', backref='discussion')
-    observation = db.relationship('Observation', backref='discussion')
+    observation = db.relationship('Observation', back_populates='discussion')
