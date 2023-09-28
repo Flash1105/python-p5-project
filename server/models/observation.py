528c0fe40ebf8b2e267629509f3cb2a5a26d895e
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-09-27 23:07:48 -0400

    edit format to flow

--- server/models/observation.py
+++ server/models/observation.py
@@ -2,9 +2,11 @@ from flask_sqlalchemy import SQLAlchemy
 from server.database import db
 
 class Observation(db.Model):
-    observation_id = db.Column(db.Integer, primary_key=True)
-    species = db.Column(db.String(150), nullable=False)
-    location = db.Column(db.String(255), nullable=False)
-    behavior = db.Column(db.String(500), nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
-   
+
+    def __init__(self, species, location, behavior, user_id, images):
+        self.species = species
+        self.location = location
+        self.behavior = behavior
+        self.user_id = user_id
+        self.images = images
+        
