e43da3817c9e41f37c539f5dc614b83d0c600358
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-09-28 01:23:13 -0400

    add relationship betweeen discussion and observ

--- server/models/observation.py
+++ server/models/observation.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from server.database import db
 
 class Observation(db.Model):
+    __tablename__ = 'observations'
     id = db.Column(db.Integer, primary_key=True)
     def __init__(self, species, location, behavior, user_id, images):
         self.species = species
@@ -9,3 +10,4 @@ class Observation(db.Model):
         self.behavior = behavior
         self.user_id = user_id
         self.images = images
+    discussions = db.relationship('Discussion', back_populates='observation')
