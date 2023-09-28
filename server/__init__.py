4c8d2a795b84c5b294feb563a9a2e9b5a30dc3c5
Alex Dulaney <18Dulaneyaw@gmail.com>
2023-09-28 10:49:55 -0400

    clean up unwanted code

--- server/__init__.py
+++ server/__init__.py
@@ -2,20 +2,20 @@ from flask import Flask
 from flask_login import LoginManager
 from server.config import Config
 from server.database import db
+import os
 
 login_manager = LoginManager()
 
-def create_app():
+def create_app(config_class=Config):
+    template_path = os.path.abspath('server/templates')
+
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     db.init_app(app)
-    
+    login_manager.init_app(app)
 
-    from auth.routes import auth_bp
-    app.register_blueprint(auth_bp)
-
-    with app.app_context():
-        db.create_all()
+    from auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
     return app
